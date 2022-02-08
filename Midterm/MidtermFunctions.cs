18fbacb89b96f461eca0966f5be6bb2864718cfb
Michael Lopez <malopez1@email.neit.edu>
2022-02-08 18:44:02 -0500

    Testing Push

--- Midterm/MidtermFunctions.cs
+++ Midterm/MidtermFunctions.cs
@@ -1,14 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using Midterm.Models;
+using Midterm;
 
 namespace Midterm
 {
-    class MidtermFunctions
+    public class MidtermFunctions
     {
-        public static Book GetBookById(intI id)
+        //Get Single book by ID
+        public static Books GetBookById(int id)
         {
+            try
+            {
+                using (var db = new SE407_BookStoreContext())
+                {
+                    return db.Books.Find(id);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
 
+            }
         }
+        //Get all books
+        //public static List<Books> GetAllBooks()
+        //{
+        //    try
+        //    {
+        //        using (var context = new SE407_BookStoreContext())
+        //        {
+        //            return context.Books.ToList();
+        //        }
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine(e.Message);
+        //        return null;
+
+        //    }
+        //}
+
+        //Get all books written by a specific authors last name
+
+
+        
+
     }
 }
+
+
---
+++ MidtermTest/MidtermBookTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Midterm;
+using MidtermTest;
+
+
+namespace MidtermTest
+{
+    public class MidtermBookTest
+    {
+        [Fact]
+        public void GetBookById()
+        {
+            var result = MidtermFunctions.GetBookById(2);
+            Assert.True(result.BookTitle == "Canterbury Tales");
+            
+        }
+
+        [Fact]
+        public void GetAllMoviesTest()
+        {
+            var result = MidtermFunctions.GetAllBooks();
+            Assert.True(result.Count == 2);
+        }
+
+
+    }
+}
